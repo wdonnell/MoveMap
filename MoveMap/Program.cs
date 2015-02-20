19c2b080d5f1a8c8c30e73595e3edfb4a7648d0a
wdonnell <cfonix@gmail.com>
2015-02-19 22:48:13 -0500

    quickly sketching it out using the whiplash plot

--- MoveMap/Program.cs
+++ MoveMap/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -7,18 +8,43 @@ using System.Threading.Tasks;
 
 namespace MoveMap
 {
-    class Program
+    class MoveMap
     {
         static void Main(string[] args)
         {
-            String input = @"stuff";
-            List<String> characters = new List<string>() { "One", "Two", "Three"};
+            String input = File.ReadAllText("./plot_summary.txt"); //taken from http://www.themoviespoiler.com/2014Spoilers/Whiplash.html
+            List<String> scenes = new List<string>(input.Split('\n'));
+            List<String> characters = new List<string>() { "Andrew", "Fletcher", "Tanner", "Metz", "Jim", "Nicole", "Connolly" };
 
+            int i = 0;
+            foreach (String scene in scenes)
+            {
+                i++;
+                var result = SceneParser.parseScene(scene, characters);
+                String toPrint = "Scene " + i + ": ";
+                foreach (var s in result)
+                {
+                    toPrint += s + ", ";
+                }
+                Console.WriteLine(toPrint);
+            }
         }
 
-        private void parsePlot(String text)
-        {
-            string[] sentences = Regex.Split(text, @"(?<=[\.!\?])\s+");
+        public static class SceneParser
+        { 
+            public static List<String> parseScene(String text, List<String> characters)
+            {
+                //string[] sentences = Regex.Split(text, @"(?<=[\.!\?])\s+");
+                List<String> toReturn = new List<string>();
+                foreach (String name in characters)
+                {
+                    if (text.Split(new char[] {' ', '.', '?', '!', ','}).Contains(name))
+                    {
+                        toReturn.Add(name);
+                    }
+                }
+                return toReturn;
+            }
         }
     }
 }
