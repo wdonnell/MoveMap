b0d5da0be71bde9be7f6c7bac8c89c0e24b34548
wdonnell <cfonix@gmail.com>
2015-02-19 21:59:10 -0500

    Initial commit

---
+++ MoveMap/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MoveMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = @"stuff";
+            List<String> characters = new List<string>() { "One", "Two", "Three"};
+
+        }
+
+        private void parsePlot(String text)
+        {
+            string[] sentences = Regex.Split(text, @"(?<=[\.!\?])\s+");
+        }
+    }
+}
