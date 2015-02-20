c8bba465c0fb1f4dfd734a5a903fe38b07464ca4
wdonnell <cfonix@gmail.com>
2015-02-20 17:37:15 -0500

    Output style is neato (graphviz) file

---
+++ MoveMap/Character.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveMap
+{
+    public class Character
+    {
+        public String name { get; set; }
+        public Dictionary<Character, int> links { get; set; }
+
+        public Character(String _name)
+        {
+            name = _name;
+            links = new Dictionary<Character, int>();
+        }
+
+        public void addLink(Character c)
+        {
+            if (this != c) //don't link to self
+            {
+                if (links.ContainsKey(c))
+                {
+                    links[c]++;
+                }
+                else
+                {
+                    links.Add(c, 1);
+                }
+            }
+        }
+    }
+}
--- MoveMap/Program.cs
+++ MoveMap/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using QuickGraph;
 
 namespace MoveMap
 {
@@ -16,7 +18,12 @@ namespace MoveMap
             String input = File.ReadAllText("./plot_summary_birdman.txt"); //taken from http://www.themoviespoiler.com/2014Spoilers/Birdman.html
             List<String> scenes = new List<string>(input.Split('\n'));
             //List<String> characters = new List<string>() { "Andrew", "Fletcher", "Tanner", "Metz", "Jim", "Nicole", "Connolly" };
-            List<String> characters = new List<string>() { "Riggan", "Birdman", "Jake", "Ralph", "Sam", "Lesley", "Laura", "Mike", "Sylvia", "Tabitha" };
+            List<String> characterNames = new List<string>() { "Riggan", "Birdman", "Jake", "Ralph", "Sam", "Lesley", "Laura", "Mike", "Sylvia", "Tabitha" };
+            List<Character> characters = new List<Character>();
+            foreach (String s in characterNames)
+            {
+                characters.Add(new Character(s));
+            }
 
             int i = 0;
             foreach (String scene in scenes)
@@ -24,25 +31,40 @@ namespace MoveMap
                 i++;
                 var result = SceneParser.parseScene(scene, characters);
                 String toPrint = "Scene " + i + ": ";
-                foreach (var s in result)
+                foreach (var a in result)
                 {
-                    toPrint += s + ", ";
+                    foreach (var b in result)
+                    {
+                        a.addLink(b);
+                    }
                 }
-                Console.WriteLine(toPrint);
             }
+            double lengthFactor = 10; //TODO: make this dependent on average or max link score.  hardcoding for now, obvs
+            //output neato file
+            String output = "strict graph G {\n";
+            foreach (Character c in characters)
+            {
+                //Console.WriteLine(c.name + ":");
+                foreach (var lin in c.links)
+                {
+                    //Console.WriteLine("\t" + lin.Key.name + " " + lin.Value);
+                    output += "\t" + c.name + " -- " + lin.Key.name + " [weight=" + lin.Value + " len= " + lengthFactor/lin.Value + "];\n";
+                }
+            }
+            output += "}\n";
+            File.WriteAllText("./output.neato", output);
         }
 
         public static class SceneParser
-        { 
-            public static List<String> parseScene(String text, List<String> characters)
+        {
+            public static List<Character> parseScene(String text, List<Character> characters)
             {
-                //string[] sentences = Regex.Split(text, @"(?<=[\.!\?])\s+");
-                List<String> toReturn = new List<string>();
-                foreach (String name in characters)
+                List<Character> toReturn = new List<Character>();
+                foreach (Character character in characters)
                 {
-                    if (text.Split(new char[] {' ', '.', '?', '!', ','}).Contains(name))
+                    if (text.Split(new char[] { ' ', '.', '?', '!', ',' }).Contains(character.name))
                     {
-                        toReturn.Add(name);
+                        toReturn.Add(character);
                     }
                 }
                 return toReturn;
